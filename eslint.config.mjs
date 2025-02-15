412b1d4f70c84d5e14088a61ebb7675e71916b4e
jimbojw <wilson.jim.r@gmail.com>
2025-02-15 14:42:25 -0500

    build: Adding Prettier support to ESLint.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,9 +8,26 @@
 // @ts-check
 
 import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  {
+    files: ["**/*.ts", "**/*.mjs", "**/*.js"],
+  },
+  {
+    ignores: ["dist"],
+  },
   eslint.configs.recommended,
-  tseslint.configs.recommended
+  tseslint.configs.recommended,
+  prettier,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
 );
