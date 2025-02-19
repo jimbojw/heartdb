fcd9151c247d4bb911447ec5686831a408527008
jimbojw <wilson.jim.r@gmail.com>
2025-02-19 09:05:25 -0500

    test: Adding more Subscription tests.

--- src/subscription.test.ts
+++ src/subscription.test.ts
@@ -21,6 +21,21 @@ interface TestDoc extends Document {
   testField: string;
 }
 
+// TODO(jimbo): Swap for p-defer library and fix jest config to work with it.
+function createPromise<ValueType = unknown>(): {
+  promise: Promise<ValueType>;
+  resolve: (value: ValueType) => void;
+  reject: (reason: unknown) => void;
+} {
+  let resolve: (value: ValueType) => void;
+  let reject: (reason: unknown) => void;
+  const promise = new Promise<ValueType>((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+  return { promise, resolve: resolve!, reject: reject! };
+}
+
 describe("Subscription", () => {
   it("should be a constructor function", () => {
     expect(Subscription).toBeInstanceOf(Function);
@@ -35,6 +50,7 @@ describe("Subscription", () => {
 
   describe("setQuery", () => {
     let heartDb: HeartDB<TestDoc>;
+    let testNumber = 0;
 
     const TEST_DOCS: TestDoc[] = new Array(100).fill(null).map((_, index) => ({
       _id: `TEST_DOC_${`${index}`.padStart(4, "0")}`,
@@ -42,11 +58,36 @@ describe("Subscription", () => {
     }));
 
     beforeEach(async () => {
+      // Create a HeartDB instance with an in-memory PouchDB instance. Then
+      // insert all TEST_DOCS and wait for all of their change notifications.
+      // This ensures that there are no lingering change notifications in the
+      // pipeline before the tests begin.
+
+      // Note: PouchDB's memory adapter seems to be shared by name. So here we
+      // give each PouchDB test database a different name, monotonically
+      // increasing with test number.
+      testNumber++;
       heartDb = new HeartDB(
-        new PouchDB<TestDoc>("TEST_DB", { adapter: "memory" }),
+        new PouchDB<TestDoc>(`TEST_DB_${testNumber}`, { adapter: "memory" }),
       );
 
+      // Monitor HeartDB changes until we've consumed one per test doc.
+      const insertedPromise = createPromise<void>();
+      let docsInserted = 0;
+      const countChange = () => {
+        docsInserted++;
+        if (docsInserted === TEST_DOCS.length) {
+          insertedPromise.resolve();
+        }
+      };
+      const disconnect = heartDb.onChange(countChange);
+
+      // Insert all test docs.
       await heartDb.pouchDb.bulkDocs(TEST_DOCS);
+
+      // Await all change notifications, then disconnect.
+      await insertedPromise.promise;
+      disconnect();
     });
 
     afterEach(() => {
@@ -124,6 +165,12 @@ describe("Subscription", () => {
     });
 
     it("should continue finding docs until all found", async () => {
+      // This test creates a query with a limit of 10 documents that matches 50
+      // docuents from the test set. Accordingly, we expect 5 calls to
+      // onEnter(). The implementation will actually perform a 6th find()
+      // request, which will return no documents and thus not trigger a 6th
+      // onEnter() call.
+
       const subscription = new Subscription(heartDb);
       expect(subscription.query).toBeUndefined();
 
@@ -152,6 +199,116 @@ describe("Subscription", () => {
       expect(subscription.query).toBe(query);
       expect(callCount).toBe(5);
       disconnect();
+    });
+
+    it("should find later added docs", async () => {
+      // This test subscribes to a query for docs beyond the initial 100, then
+      // adds a new doc that matches the query. We expect the onEnter() callback
+      // to be invoked only once because the initial query finds no documents.
+
+      const subscription = new Subscription(heartDb);
+      expect(subscription.query).toBeUndefined();
+
+      const deferred = createPromise<void>();
+
+      const disconnect = subscription.onEnter((enterEvent) => {
+        const docs = enterEvent.detail;
+        expect(Object.keys(docs).length).toBe(1);
+        deferred.resolve();
+      });
+
+      // Note: Initial query is beyond the 100 initial docs.
+      const query: PouchDB.Find.FindRequest<TestDoc> = {
+        selector: { _id: { $gte: "TEST_DOC_0100" } },
+      };
+
+      await subscription.setQuery(query);
+
+      heartDb.pouchDb.put({
+        _id: "TEST_DOC_0101",
+        testField: "test value 101",
+      });
+
+      await deferred.promise;
+      disconnect();
+    });
+
+    it("should detect updated docs", async () => {
+      // This test modifies test docs 98 and 99 and expects the onUpdate()
+      // callback to be invoked once for each.
+
+      const subscription = new Subscription(heartDb);
+
+      // Track call to entering documents.
+      const enterDeferred = createPromise<void>();
+      const enterDisconnect = subscription.onEnter((enterEvent) => {
+        // Expect initial query to find documents 98 and 99.
+        const docs = enterEvent.detail;
+        expect(Object.keys(docs).length).toBe(2);
+
+        // Expect docs' _rev field to start with "1-".
+        for (const id in docs) {
+          expect(docs[id]._rev).toMatch(/^1-/);
+        }
+
+        enterDeferred.resolve();
+        enterDisconnect();
+      });
+
+      let updateCallCount = 0;
+      const updateDeferred = createPromise<void>();
+
+      const updateDisconnect = subscription.onUpdate((updateEvent) => {
+        updateCallCount++;
+        const docs = updateEvent.detail;
+
+        expect(Object.keys(docs).length).toBe(1);
+
+        const doc = docs[Object.keys(docs)[0]];
+
+        switch (updateCallCount) {
+          case 1:
+            // First update call should find modified doc 98.
+            expect(doc._rev).toMatch(/^2-/);
+            expect(doc.testField).toBe("updated value 98");
+            break;
+          case 2:
+            // Second and final update call should find updated doc 99.
+            expect(doc._rev).toMatch(/^2-/);
+            expect(doc.testField).toBe("updated value 99");
+            updateDeferred.resolve();
+            updateDisconnect();
+            break;
+          default:
+            // No further calls expected.
+            throw new Error("Unexpected call to onUpdate");
+        }
+      });
+
+      const query: PouchDB.Find.FindRequest<TestDoc> = {
+        selector: { _id: { $gte: "TEST_DOC_0098" } },
+      };
+
+      await subscription.setQuery(query);
+
+      // Wait for the entering docs to be notified.
+      await enterDeferred.promise;
+
+      // Modify TEST_DOC_098.
+      const testDoc98Orig = await heartDb.pouchDb.get("TEST_DOC_0098");
+      await heartDb.pouchDb.put({
+        ...testDoc98Orig,
+        testField: "updated value 98",
+      });
+
+      // Modify TEST_DOC_099.
+      const testDoc99Orig = await heartDb.pouchDb.get("TEST_DOC_0099");
+      await heartDb.pouchDb.put({
+        ...testDoc99Orig,
+        testField: "updated value 99",
+      });
+
+      await updateDeferred.promise;
     });
   });
 });
